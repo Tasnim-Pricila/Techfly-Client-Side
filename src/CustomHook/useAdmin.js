6f68c66f9e37ca48bb6be6345781b5ae8eaa62dd
unknown <tasnimtanzim55@gmail.com>
2022-05-23 02:26:23 +0600

    Only admin can make admin implemented

---
+++ src/CustomHook/useAdmin.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const useAdmin = (user) => {
+
+    const [admin, setAdmin] = useState('');
+    const email = user?.email;
+
+
+    if(email){
+        fetch(`http://localhost:5000/user/${email}`, {
+            method:'GET',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json()) 
+        .then(data=> setAdmin(data.admin))
+    }
+
+    return [admin];
+};
+
+export default useAdmin;
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import useAdmin from '../../CustomHook/useAdmin';
+import auth from '../../firebase.init';
 
 const Dashboard = () => {
+
+    const [user, loading] = useAuthState(auth);
+    const [admin] = useAdmin(user);
+    console.log(user.email)
+
+    if (loading) {
+        <p>Loading...</p>
+    }
+
     return (
         <div>
             <div className="drawer drawer-mobile">
@@ -13,13 +25,33 @@ const Dashboard = () => {
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                        <li><Link to='/dashboard'>My Orders</Link></li>
-                        <li><Link to='/dashboard/review'>Add a Review</Link></li>
-                        <li><Link to='/dashboard/manageOrders'>Manage All Orders</Link></li>
-                        <li><Link to='/dashboard/addProduct'>Add a Product</Link></li>
-                        <li><Link to='/dashboard/makeAdmin'>Make Admin</Link></li>
-                        <li><Link to='/dashboard/manageProducts'>Manage Products</Link></li>
-                        <li><Link to='/dashboard/myProfile'>My Profile</Link></li>
+                        {!admin &&
+                            <>
+                                <li><Link to='/dashboard'>My Orders</Link></li>
+                                <li><Link to='/dashboard/review'>Add a Review</Link></li>
+                            </>
+                        }
+                        {
+                            admin &&
+                            <>
+                                <li>
+                                    <Link to='/dashboard/manageOrders'>Manage All Orders</Link>
+                                </li>
+                                <li>
+                                    <Link to='/dashboard/addProduct'>Add a Product</Link>
+                                </li>
+                                <li>
+                                    <Link to='/dashboard/makeAdmin'>Make Admin</Link>
+                                </li>
+                                <li>
+                                    <Link to='/dashboard/manageProducts'>Manage Products</Link>
+                                </li>
+
+                            </>
+                        }
+                        <li>
+                            <Link to='/dashboard/myProfile'>My Profile</Link>
+                        </li>
                     </ul>
                 </div>
             </div>
