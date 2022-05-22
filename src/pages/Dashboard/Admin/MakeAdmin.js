9997e4b48c9d8504f22a80b37c21354f2e60e011
unknown <tasnimtanzim55@gmail.com>
2022-05-23 01:17:40 +0600

    All USers get from database

--- src/pages/Dashboard/Admin/MakeAdmin.js
+++ src/pages/Dashboard/Admin/MakeAdmin.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
 const MakeAdmin = () => {
+    const { data: users, isLoading, refetch } = useQuery('users', () =>
+        fetch('http://localhost:5000/users', {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json()))
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
     return (
         <div>
-            
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table table-zebra w-full text-center">
+                        <thead>
+                            <tr>
+                                <th> No.</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map((user, index) =>
+                                    <tr key={user._id}>
+                                        <th>{index + 1}</th>
+                                        <td className='font-semibold text-secondary'> {user.email} </td>
+                                        <td>
+                                            <button className='btn btn-success'>Make Admin</button>
+                                        </td>  
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
         </div>
     );
 };
